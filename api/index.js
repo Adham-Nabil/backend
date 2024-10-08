86389c4fdf5e086fa2ba84138d3ab040554a4b72
Adham <adhammohammed151@gmail.com>
2024-10-09 01:51:03 +0300

    cors

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
-// const express = require("express");
-// const cors = require("cors");
+const express = require("express");
+const cors = require("cors");
 
 require("dotenv").config();
 
-// const app = express();
-// app.use(cors());
+const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.API_KEY;
